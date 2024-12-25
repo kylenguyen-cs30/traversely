0185839edf6c61065c359d43df5e05e07861ef43
Kyle Nguyen <knguyen1193@outlook.com>
2024-12-25 13:19:45 -0800

    temp commit. fix later

--- src/types/graph.ts
+++ src/types/graph.ts
@@ -1,3 +1,4 @@
+
 export interface Position {
   x: number;
   y: number;
@@ -6,4 +7,24 @@ export interface Node {
   id: string;
   position: Position;
   value: string | number;
+  visited?: boolean;
+  distance: number;
+  previousNode?: string;
+}
+
+export interface Edge {
+  id: string;
+  source: string;
+  target: string;
+  weight?: number;
+  visited?: boolean;
+}
+
+export type NodeMap = Map<string, Node>;
+export type EdgeMap = Map<string, Edge>;
+
+export interface GraphState {
+  nodes: NodeMap;
+  edges: EdgeMap;
+
 }
