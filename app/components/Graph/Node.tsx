79a23604528cd2512169fe18f5bca996d074cef3
Kyle Nguyen <knguyen1193@outlook.com>
2025-04-07 15:51:25 -0700

    adding custom css in styles folder

--- app/components/Graph/Node.tsx
+++ app/components/Graph/Node.tsx
@@ -1,28 +1,33 @@
 "use client";
 
 import { memo } from "react";
-import { Handle, Position, NodeProps, Handle, Handle } from "@xyflow/react";
-import styles from "@/app/styles/Graph.module.scss";
+import { Handle, Position, NodeProps } from "@xyflow/react";
+import styles from "@/app/styles/Node.module.scss";
+
 interface CustomNodeData {
   label: string;
-}
-
-const CustomNodeData{
-  label: string;
+  isVisited?: boolean;
+  isCurrent?: boolean;
 }
 
 const CustomNode = ({ data }: NodeProps<CustomNodeData>) => {
-  const { label } = data;
+  const { label, isVisited, isCurrent } = data;
+
+  let nodeContentClass = styles.nodeContent;
+  if (isCurrent) {
+    nodeContentClass += ` ${styles.nodeCurrent}`;
+  } else if (isVisited) {
+    nodeContentClass += ` ${styles.nodeVisited}`;
+  }
   return (
     <>
-
       <Handle type="target" position={Position.Top}></Handle>
-      <div className={styles.nodeContent}>
+      <div className={nodeContentClass}>
         <div className={styles.nodeLabel}>{label}</div>
       </div>
       <Handle type="source" position={Position.Bottom}></Handle>
     </>
-  )
-}
+  );
+};
 
-export default memo(CustomNode)
+export default memo(CustomNode);
